3ab93e09fd3c1ee466b013628f2e3b27e4429f85
Pawel <pawel@Pawels-MacBook-Pro.local>
2016-01-13 19:06:57 -0500

    adding support for authentication

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'pry'
 # adding Devise for users management and authentication
 gem 'devise'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  after_filter :set_access_control_headers
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
